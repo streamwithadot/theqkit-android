eca2f798df42b92aa88776ea5ac3819edbead999
Tyler Roach <tyler@appterial.com>
2021-09-09 10:26:43 -0400

    1.0.7

--- theqkit/src/main/java/live/stream/theq/theqkit/data/sdk/AuthData.kt
+++ theqkit/src/main/java/live/stream/theq/theqkit/data/sdk/AuthData.kt
@@ -5,7 +5,7 @@ import kotlinx.android.parcel.Parcelize
 import live.stream.theq.theqkit.data.app.Device
 
 /** @suppress **/
-@Parcelize data class FirebaseLogin(val id: String, val accessToken: String): Parcelable
+@Parcelize data class FirebaseLogin(val id: String, val accessToken: String, val email: String? = null): Parcelable
 
 /** @suppress **/
 @Parcelize data class LoginAuthData(
